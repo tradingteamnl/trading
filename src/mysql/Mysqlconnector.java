b8a3f0142cee856fb300072baebb096094b8d177
michel <micheldaniel2@hotmail.com>
2017-02-25 00:42:28 +0100

    extra optie
    een extra optie gemaakt voor mysql

--- src/mysql/Mysqlconnector.java
+++ src/mysql/Mysqlconnector.java
@@ -29,4 +29,26 @@ public class Mysqlconnector {
     public String getUrlmysql(){
         return "jdbc:mysql://"+IPADDRESS+":"+POORT+"/"+DATABASENAAM+"?autoReconnect="+AUTORECONNECT+"&useSSL="+SSL;
     };
+
+
+    private final String USENAMESECONDE = "root";
+    private final String PASSWORDSECONDE = "Pulsar11";
+    private final String IPADDRESSSECONDE = "25.100.87.213";
+    private final String POORTSECONDE = "3306";
+    private final String DATABASENAAMSECONDE = "cryptoData2_0";
+    private final boolean AUTORECONNECTSECONDE = true;
+    private final boolean SSLSECONDE = false;
+    
+    public String getUsernameSeconde(){
+        return USENAMESECONDE;
+    };
+        
+    public String getPasswordSeconde(){
+        return PASSWORDSECONDE;
+    };
+    
+    public String getUrlmysqlSeconde(){
+        return "jdbc:mysql://"+IPADDRESSSECONDE+":"+POORTSECONDE+"/"+DATABASENAAMSECONDE+"?autoReconnect="+AUTORECONNECTSECONDE+"&useSSL="+SSLSECONDE;
+    };
+
 }
