ea7c8fb14d254e521e1804e8697ff1c6b7c88fb4
michel <micheldaniel2@hotmail.com>
2017-03-06 11:51:40 +0100

    verwijderd import
    verwijderd import die niet gebruikt word

--- src/global/FileSystem.java
+++ src/global/FileSystem.java
@@ -9,7 +9,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileSystemView;
